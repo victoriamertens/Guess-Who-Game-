e6a862216386fa9a7b561252f56e825b02eb2749
Victoria Mertens <victoria.mertens44@gmail.com>
2022-10-31 16:35:20 -0500

    Planned out approach in comments

--- client.js
+++ client.js
@@ -1,1 +1,33 @@
 console.log('Here are all the available people:', people);
+$(document).ready(onReady); 
+
+
+function onReady(){ 
+  console.log("DOM rendered"); 
+  //call intialRender in the onReady funcion to create the DOM 
+//event listener for rendered event - click on photo, run clickedPhoto
+} 
+
+
+//initialRender function 
+//create a global variable randomNumber
+// run (randomNumber(min, max(people.length-1)))
+// update the global variable randomNumber to it 
+//apend Click on: people[ at randomNumber]
+//loop through each of the people in the people array 
+//append img for each photo infomation into html
+//<img src="https://github.com/christopher-black.png?size=250" alt="Profile image of Chris">
+//include a data.id for each, perhaps that corresponds to index  
+
+
+//randomNumber function 
+//function randomNumber(min, max){
+//  return Math.floor(Math.random() * (1 + max - min) + min);
+//}
+
+//function clickedPhoto (){}
+//grab the information from the clicked item, data.id
+//set that information equal to a local variable
+//run a conditional, test if local variable is equal to randomNumber 
+//if true create an alert that says correct and tells to refresh page 
+//if false alert try again 
--- data.js
+++ data.js
@@ -2,29 +2,36 @@ var people = [
   {
     name: 'Dev',
     githubUsername: 'devjanaprime'
+    imgSource: 'https://avatars.githubusercontent.com/u/17906158?v=4'
   },
   {
     name: 'Dane',
     githubUsername: 'DoctorHowser'
+    imgSource: 'https://avatars.githubusercontent.com/u/11574995?v=4'
   },
   {
     name: 'Matt',
     githubUsername: 'matthew-black'
+    imgSource: 'https://avatars.githubusercontent.com/u/5817534?v=4'
   },
   {
     name: 'Kris',
     githubUsername: 'kdszafranski'
+    imgSource: 'https://avatars.githubusercontent.com/u/5541481?v=4'
   },
   {
     name: 'Edan',
     githubUsername: 'eschwartz'
+    imgSource: 'https://avatars.githubusercontent.com/u/1153371?v=4'
   },
   {
     name: 'Liz',
     githubUsername: 'emkerber'
+    imgSource: 'https://avatars.githubusercontent.com/u/17734101?v=4'
   },
   {
     name: 'Chris',
     githubUsername: 'christopher-black'
+    imgSource: 'https://avatars.githubusercontent.com/u/25748592?v=4'
   },
 ];
