19e3b58d41353d7ce54a6f6979b84f50c21e21a7
Victoria Mertens <victoria.mertens44@gmail.com>
2022-10-31 16:56:01 -0500

    finished the render

--- client.js
+++ client.js
@@ -5,25 +5,35 @@ $(document).ready(onReady);
 function onReady(){ 
   console.log("DOM rendered"); 
   //call intialRender in the onReady funcion to create the DOM 
+  initialRender(); 
 //event listener for rendered event - click on photo, run clickedPhoto
 } 
 
-
-//initialRender function 
+let randomNumber = 0; 
+function initialRender (){ 
 //create a global variable randomNumber
 // run (randomNumber(min, max(people.length-1)))
+randomNumber = randomNumberFunction(0,people.length-1); 
 // update the global variable randomNumber to it 
+console.log(randomNumber);
 //apend Click on: people[ at randomNumber]
+$('#starting-people').append(`
+<h2>Click on: ${people[randomNumber].name}</h2>
+`)
 //loop through each of the people in the people array 
 //append img for each photo infomation into html
-//<img src="https://github.com/christopher-black.png?size=250" alt="Profile image of Chris">
-//include a data.id for each, perhaps that corresponds to index  
+for (let i = 0; i<people.length; i++){ 
+  $('#starting-people').append(`
+  <div><img src="${people[i].imgSource}" data.id="${i}"></div>
+  `);
+}
+//include a data.id for each, perhaps that corresponds to index
+} 
 
+function randomNumberFunction (min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+   } 
 
-//randomNumber function 
-//function randomNumber(min, max){
-//  return Math.floor(Math.random() * (1 + max - min) + min);
-//}
 
 //function clickedPhoto (){}
 //grab the information from the clicked item, data.id
